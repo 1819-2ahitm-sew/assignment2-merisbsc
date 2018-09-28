c8921e909056491b9a7b525c50980cb8733cf2a1
merisbsc <m.besic@students.htl-leonding.ac.at>
2018-09-28 10:44:12 +0200

    Meris, Programm fertig

--- Robot/src/main/java/Main.java
+++ Robot/src/main/java/Main.java
@@ -1,8 +1,31 @@
+import at.htl.robot.model.Robot;
+
 public class Main {
 
     public static void main(String[] args) {
 
         // Do something with the robot
+
+        Robot robot = new Robot();
+
+        System.out.println("x = " + robot.getX());
+        System.out.println("y = " + robot.getY());
+        System.out.println("Direction = " + robot.getDirection());
+
+        System.out.println();
+        robot.stepForward();
+
+        System.out.println("x = " + robot.getX());
+        System.out.println("y = " + robot.getY());
+        System.out.println("Direction = " + robot.getDirection());
+
+        System.out.println();
+        robot.rotateLeft();
+        robot.rotateLeft();
+
+        System.out.println("x = " + robot.getX());
+        System.out.println("y = " + robot.getY());
+        System.out.println("Direction = " + robot.getDirection());
 
     }
 
--- Robot/src/main/java/at/htl/robot/model/Robot.java
+++ Robot/src/main/java/at/htl/robot/model/Robot.java
@@ -2,5 +2,71 @@ package at.htl.robot.model;
 
 public class Robot {
 
+    private int x = 0;
+    private int y = 0;
+    private Direction direction = Direction.SOUTH;
 
+
+    //region getter and setter
+
+    public int getX() {
+        return x;
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
+
+    public Direction getDirection() {
+        return direction;
+    }
+
+    public void setDirection(Direction direction) {
+        this.direction = direction;
+    }
+    //endregion
+
+    public int stepForward() {
+        switch (direction) {
+            case SOUTH:
+                y += 1;
+                return y;
+            case NORTH:
+                y -= 1;
+                return y;
+            case WEST:
+                x -= 1;
+                return x;
+            case EAST:
+                x += 1;
+                return x;
+        }
+        return x;
+    }
+
+    public Direction rotateLeft() {
+        switch (this.direction) {
+            case NORTH:
+                this.direction = Direction.WEST;
+                break;
+            case WEST:
+                this.direction = Direction.SOUTH;
+                break;
+            case SOUTH:
+                this.direction = Direction.EAST;
+                break;
+            case EAST:
+                this.direction = Direction.NORTH;
+                break;
+        }
+        return this.direction;
+    }
 }
